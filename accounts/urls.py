a014d242315e03063f9e2525484598ecf84baa80
muhammed rila <muhammedrilak1332@gamil.com>
2025-07-28 13:45:09 +0530

    first commit

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-from .views import RegisterView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import register_user
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/',register_user),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,19 @@
-from rest_framework import generics
-from .models import CustomUser
-from .serializers import RegisterSerializer
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view,permission_classes
+from rest_framework.permissions import AllowAny,IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
-class RegisterView(generics.CreateAPIView):
-    queryset = CustomUser.objects.all()
-    serializer_class = RegisterSerializer
+@api_view(['POST'])
+@permission_classes(['AllowAny'])
+def register_user(request):
+    username=request.data.get('username')
+    email=request.data.get('email')
+    password=request.data.get('password')
+
+    if not username or password:
+        return Response({'error':'username and password are required'},status=status.HTTP_400_BAD_REQUEST)
+    if User.objects.filter(username=username).exists():
+        return Response({'error':'username alredy exists'},status=status.HTTP_400_BAD_REQUEST)
+    User=User.objects.create_user(username=username, email=email, password=password)
+    return Response({'msg':'user generated succesfully'},status=status.HTTP_201_CREATED)
